95e29a813b466b5617f37513a8b679ac271baa72
dsvoid <dmitry@ds0.xyz>
2021-04-16 17:04:17 -0400

    #5 player will linearly interpolate towards selected object

--- scenes and scripts/Player.gd
+++ scenes and scripts/Player.gd
@@ -7,6 +7,8 @@ var stop_accel : float = 45.0
 var rot_accel : float = 8.0
 var vel : Vector3 = Vector3()
 var grab_dist : float = 1.9
+var target_grab_pos : Vector3 = Vector3.ZERO
+var found_grab_pos : bool = false
 
 onready var camera_rig = $CameraRig
 onready var camera = $CameraRig/Camera
@@ -18,19 +20,23 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_pressed("use_key") and $GrabRayCast.is_colliding():
-		process_grab()
+		process_grab(delta)
 	elif Input.is_action_just_released("use_key") and $GrabRayCast.is_colliding():
 		process_release()
+		print("released")
 	else:
 		process_movement(delta)
 	camera_follows_player()
 
-func process_grab():
+func process_grab(delta):
 	# tint grabbed object
 	var collider = $GrabRayCast.get_collider()
 	var new_mat = SpatialMaterial.new()
 	new_mat.albedo_color = Color(0,0.7,0)
 	collider.get_node("MeshInstance").material_override = new_mat
+	# move player to object and rotate them towards it as well
+	# ... oh shit
+	move_player_against_object(collider,delta)
 
 func process_release():
 	# tint released object
@@ -38,6 +44,38 @@ func process_release():
 	var new_mat = SpatialMaterial.new()
 	new_mat.albedo_color = Color(1.0,0.6,1.0)
 	collider.get_node("MeshInstance").material_override = new_mat
+	found_grab_pos = false
+
+func move_player_against_object(collider,delta):
+	var obj_pos = collider.global_transform.origin
+	var player_pos = translation
+	if not found_grab_pos:
+		find_grab_pos(player_pos,obj_pos)
+		found_grab_pos = true
+	var look_dir = Vector3(player_pos.x - obj_pos.x, 0, player_pos.z - obj_pos.z)
+	rotation.y = lerp_angle(rotation.y, atan2(-look_dir.x,-look_dir.z), delta*rot_accel)
+	translation = lerp(translation,target_grab_pos,0.15)
+	if abs(translation.x - target_grab_pos.x) < 0.01:
+		translation.x = target_grab_pos.x
+	if abs(translation.z - target_grab_pos.z) < 0.01:
+		translation.z = target_grab_pos.z
+
+func find_grab_pos(player_pos,obj_pos):
+	print("PLAYER: "+str(player_pos))
+	print("OBJ: "+str(obj_pos))
+	if abs(player_pos.x - obj_pos.x) > abs(player_pos.z - obj_pos.z):
+		target_grab_pos.z = obj_pos.z
+		if player_pos.x < obj_pos.x:
+			target_grab_pos.x = obj_pos.x - 1
+		else:
+			target_grab_pos.x = obj_pos.x + 1
+	else:
+		target_grab_pos.x = obj_pos.x
+		if player_pos.z < obj_pos.z:
+			target_grab_pos.z = obj_pos.z - 1
+		else:
+			target_grab_pos.z = obj_pos.z + 1
+	print("TARGET GRAB POS: "+str(target_grab_pos))
 
 func process_movement(delta):
 	var input_dir = Vector3.ZERO
